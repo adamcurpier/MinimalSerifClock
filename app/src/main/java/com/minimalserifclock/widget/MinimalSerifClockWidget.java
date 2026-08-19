97d4aafd56f4bc0b76778b1753e00a778d193f0f
adamcurpier <117946468+adamcurpier@users.noreply.github.com>
2026-08-19 14:48:02 -0400

    Open alarm clock when widget is tapped

--- app/src/main/java/com/minimalserifclock/widget/MinimalSerifClockWidget.java
+++ app/src/main/java/com/minimalserifclock/widget/MinimalSerifClockWidget.java
@@ -1,15 +1,27 @@
 package com.minimalserifclock.widget;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
+import android.provider.AlarmClock;
 import android.widget.RemoteViews;
 
 public class MinimalSerifClockWidget extends AppWidgetProvider {
     @Override
     public void onUpdate(Context context, AppWidgetManager manager, int[] ids) {
+        Intent alarmIntent = new Intent(AlarmClock.ACTION_SHOW_ALARMS);
+        PendingIntent alarmPendingIntent = PendingIntent.getActivity(
+                context,
+                0,
+                alarmIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE
+        );
+
         for (int id : ids) {
             RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_clock);
+            views.setOnClickPendingIntent(R.id.widget_root, alarmPendingIntent);
             manager.updateAppWidget(id, views);
         }
     }
